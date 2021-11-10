288c0f494935b039e4796d1b7948002d7f0f333d
Marco <88397083+Marcokbc@users.noreply.github.com>
2021-11-10 15:00:39 -0300

    Finalizando evento de movimento com um personagem

--- JS - PPA/Aula6/index.js
+++ JS - PPA/Aula6/index.js
@@ -1,7 +1,10 @@
-let quadrado = document.querySelector("#quadrado");
-let quadradoX = 700; //Posicionamento horizontal
-let quadradoY = 200;
-let quadradoTamanho = 100;
+let personagem = document.querySelector("#personagem");
+let personagemImg = document.querySelector("#personagem img");
+let personagemX = 200; //Posicionamento horizontal
+let personagemY = 200;
+let personagemLargura = 75;
+let personagemAltura = 90;
+let velocidade = 10;
 
 let alturaContainer = 600;
 let larguraContainer = 800;
@@ -11,32 +14,54 @@ document.addEventListener("keydown",(e)=>{
     let tecla = e.key;
     switch(tecla){
         case 'ArrowUp':
-            quadradoY -= 100;
-            if(quadradoY <= 0){
-                quadradoY = 0;
+            personagemImg.classList.add("movimento","paracima");
+            personagemImg.classList.remove("parabaixo","paraesquerda","paradireita");
+            personagemY -= velocidade;
+            if(personagemY <= 0){
+                personagemY = 0;
             }
-            quadrado.style.top = quadradoY+"px";
+            personagem.style.top = personagemY+"px";
         break;
         case 'ArrowDown':
-            quadradoY += 100;
-            if(quadradoY >= alturaContainer - quadradoTamanho){
-                quadradoY = alturaContainer - quadradoTamanho;
+            personagemImg.classList.add("movimento","parabaixo");
+            personagemImg.classList.remove("paracima","paraesquerda","paradireita");
+            personagemY += velocidade;
+            if(personagemY >= alturaContainer - personagemAltura){
+                personagemY = alturaContainer - personagemAltura;
             }
-            quadrado.style.top = quadradoY+"px";
+            personagem.style.top = personagemY+"px";
         break;
         case 'ArrowLeft':
-            quadradoX -= 100;
-            if(quadradoX <= 0){
-                quadradoX = 0;
+            personagemImg.classList.add("movimento","paraesquerda");
+            personagemImg.classList.remove("parabaixo","paracima","paradireita");
+            personagemX -= velocidade;
+            if(personagemX <= 0){
+                personagemX = 0;
             }
-            quadrado.style.left = quadradoX+"px";
+            personagem.style.left = personagemX+"px";
         break;
         case 'ArrowRight':
-            quadradoX += 100;
-            if(quadradoX >= larguraContainer - quadradoTamanho){
-                quadradoX = larguraContainer - quadradoTamanho;
+            personagemImg.classList.add("movimento","paradireita");
+            personagemImg.classList.remove("parabaixo","paraesquerda","paracima");
+            personagemX += velocidade;
+            if(personagemX >= larguraContainer - personagemLargura){
+                personagemX = larguraContainer - personagemLargura;
             }
-            quadrado.style.left = quadradoX+"px";
+            personagem.style.left = personagemX+"px";
+        break;
+    }
+});
+document.addEventListener("keyup",(e)=>{
+    e.preventDefault();
+
+    let tecla = e.key;
+
+    switch(tecla){
+        case 'ArrowUp':
+        case 'ArrowDown':
+        case 'ArrowLeft':
+        case 'ArrowRight':
+            personagemImg.classList.remove("movimento");
         break;
     }
 });
